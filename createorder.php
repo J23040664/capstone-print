89aec0991d98c5b757d3def11ac014cda6f92ab2
Yong-0813 <yonglum08@gmail.com>
2025-06-09 15:15:56 +0800

    s

--- createorder.php
+++ createorder.php
@@ -183,10 +183,18 @@
           <div class="mb-4">
             <label for="typeservices" class="form-label">Type Of Services:</label>
             <select class="form-select" id="serviceType" name="serviceType">
-              <option value="None">None</option>
-              <option value="Lamination">Lamination</option>
-              <option value="Stapler">Stapler</option>
-            </select>
+              <option value="None">-- Select Service --</option>
+              <?php
+              // Include your database connection
+              include 'dbms.php'; // make sure this file connects to your DB
+
+              $sql = "SELECT service_id, service_type FROM service_list WHERE service_status = 'Available'";
+              $result = mysqli_query($conn, $sql);
+
+              while ($row = mysqli_fetch_assoc($result)) {
+                  echo "<option value='" . $row['service_id'] . "'>" . $row['service_type'] . "</option>";
+              }
+              ?>
           </div>
 
           <!-- Print Color Option -->
