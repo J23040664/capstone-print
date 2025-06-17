ebe1052ae1879612fb04211ca9f9d9437e18804f
J23040664 <j23040664@student.newinti.edu.my>
2025-06-18 07:50:26 +0800

    update

--- logout.php
+++ logout.php
@@ -4,7 +4,7 @@ session_unset();
 session_destroy();
 
 $logoutMessage = "You have successfully logged out!<br>";
-header("refresh:2; url=login.php");
+// header("refresh:2; url=login.php");
 
 ?>
 
@@ -16,26 +16,17 @@ header("refresh:2; url=login.php");
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Logout</title>
 
-    <link rel="stylesheet" href="./style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.12.1/font/bootstrap-icons.min.css">
-
-    <style>
-        body {
-            background-color: #f2f2f5;
-            min-height: 100vh;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-        }
-    </style>
+    <link rel="stylesheet" href="./adminStyle.css">
 </head>
 
-<body>
+<body class="login-body">
 
-    <div class="login-box">
-        <h3 class="text-center mb-3">Logout</h3>
-        <p class="text-center"><?php echo $logoutMessage; ?></p>
+    <div class="logout-box">
+        <h2 class="text-center mb-3">Logout</h2>
+        <p class="text-center text-danger fs-3"><?php echo $logoutMessage; ?></p>
+        <a class="btn login-btn d-block text-center" href=" login.php">Back to login</a>
     </div>
 
 </body>
